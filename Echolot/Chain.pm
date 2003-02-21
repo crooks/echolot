0008f8c4859ed305718566d18a15a747a875413e
Peter Palfrader <peter@palfrader.org>
2003-02-21 13:28:20 +0000

    Allow chaingpinging A,A if explicitly requested

--- Echolot/Chain.pm
+++ Echolot/Chain.pm
@@ -1,7 +1,7 @@
 package Echolot::Chain;
 
 # (c) 2002 Peter Palfrader <peter@palfrader.org>
-# $Id: Chain.pm,v 1.12 2003/02/20 19:46:24 weasel Exp $
+# $Id: Chain.pm,v 1.13 2003/02/21 13:28:20 weasel Exp $
 #
 
 =pod
@@ -165,7 +165,7 @@ sub send_pings($;$$) {
 			my $key1 = get_latest_key($addr1, $type1);
 
 			for my $rem2 (@remailers) {
-				next if $rem1 eq $rem2;
+				next if $rem1 eq $rem2 && (! $which1 eq $rem1 && $which2 eq $rem2);
 				next unless $rem2->{'pingit'};
 				my $addr2 = $rem2->{'address'};
 
